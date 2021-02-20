49f04682e56ef9f80d2b81dcbf569d540139dbb2
Mark van der Salm <mark.van.der.salm@gmail.com>
2021-02-20 13:44:34 +0100

    Rollup output now bundles TypeScript type definitions

--- react-redux-loading-hook/rollup.config.js
+++ react-redux-loading-hook/rollup.config.js
@@ -2,27 +2,35 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
-module.exports = {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-  ],
-};
+module.exports = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true }),
+    ],
+  },
+  {
+    input: ".build/tsc/index.d.ts",
+    output: [{ file: ".build/dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
